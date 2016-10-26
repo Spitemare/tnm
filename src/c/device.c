26d34ed8777488b835cb9c0a59f3074b5a3e9878
David Morgan <dmorgan81@gmail.com>
2016-10-26 07:55:18 -0400

    Settings framework

--- src/c/device.c
+++ src/c/device.c
@@ -1,6 +1,10 @@
 #include <pebble.h>
+#include <pebble-events/pebble-events.h>
+#include <pebble-connection-vibes/connection-vibes.h>
+#include <pebble-hourly-vibes/hourly-vibes.h>
 #include "logging.h"
 #include "fonts.h"
+#include "enamel.h"
 #include "minute_layer.h"
 #include "hour_layer.h"
 #include "battery_layer.h"
@@ -9,6 +13,18 @@ static Window *s_window;
 static MinuteLayer *s_minute_layer;
 static HourLayer *s_hour_layer;
 static BatteryLayer *s_battery_layer;
+
+static EventHandle s_settings_event_handle;
+
+static void settings_handler(void *context) {
+    log_func();
+    connection_vibes_set_state(atoi(enamel_get_CONNECTION_VIBE()));
+    hourly_vibes_set_enabled(enamel_get_HOURLY_VIBE());
+#ifdef PBL_HEALTH
+    connection_vibes_enable_health(enamel_get_ENABLE_HEALTH());
+    hourly_vibes_enable_health(enamel_get_ENABLE_HEALTH());
+#endif
+}
 
 static void window_load(Window *window) {
     log_func();
@@ -27,10 +43,15 @@ static void window_load(Window *window) {
 
     s_battery_layer = battery_layer_create(bounds);
     layer_add_child(root_layer, s_battery_layer);
+
+    settings_handler(NULL);
+    s_settings_event_handle = enamel_settings_received_subscribe(settings_handler, NULL);
 }
 
 static void window_unload(Window *window) {
     log_func();
+    enamel_settings_received_unsubscribe(s_settings_event_handle);
+
     battery_layer_destroy(s_battery_layer);
     hour_layer_destroy(s_hour_layer);
     minute_layer_destroy(s_minute_layer);
@@ -38,7 +59,17 @@ static void window_unload(Window *window) {
 
 static void init(void) {
     log_func();
+    enamel_init();
     fonts_init();
+    connection_vibes_init();
+    hourly_vibes_init();
+    uint32_t const pattern[] = { 100 };
+    hourly_vibes_set_pattern((VibePattern) {
+        .durations = pattern,
+        .num_segments = 1
+    });
+
+    events_app_message_open();
 
     s_window = window_create();
     window_set_window_handlers(s_window, (WindowHandlers) {
@@ -52,7 +83,10 @@ static void deinit(void) {
     log_func();
     window_destroy(s_window);
 
+    hourly_vibes_deinit();
+    connection_vibes_deinit();
     fonts_deinit();
+    enamel_deinit();
 }
 
 int main(void) {
